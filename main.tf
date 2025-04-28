6980f7443b3aa26e83ed779a22d2974a281e4810
satishsahu25 <satishk7619@gmail.com>
2025-04-28 21:19:26 +0530

    third

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 //devloper-2 running the module ec2 instance with parameters ami_value, instance_type
 //also u have to provide location of the modules.
-module "ec2_instance" {
+module "ec2instance" {
   source="/modules/ec2instance"
   ami_value = "ami-0e35ddab05955cf57"
   instance_type="t2.micro"
